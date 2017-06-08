949a55b52ed6e1d54bce714ce8d56ecb4de54f37
yhy <yhy@yhydeMacBook-Pro.local>
2017-06-08 23:37:19 +0800

    添加css样式和js效果

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yhy on 2017/6/5.
+ */
+$(document).ready(function () {
+   $('.yx-chart').hover(function() {
+       $(this).animate({top: '-96px'});
+   });
+   $('.ranking').mousemove(function() {
+       $()
+   })
+});
+
+$(document).ready(function () {
+   $('.yx-chart').mouseout(function() {
+       $(this).animate({top: '0'});
+   });
+   // $('.ranking').hover(function() {
+   //     $(this).find('.imgPack').addClass('hover');
+   // })
+});
