4711d1422c84e28979f991eb8a7dda2818c197ae
yhy <yhy@yhydeMacBook-Pro.local>
2017-06-11 00:01:17 +0800

    优化js网页效果

--- static/js/index.js
+++ static/js/index.js
@@ -2,19 +2,102 @@
  * Created by yhy on 2017/6/5.
  */
 $(document).ready(function () {
-   $('.yx-chart').hover(function() {
-       $(this).animate({top: '-96px'});
+   $('.groom ul li').mouseenter(function() {
+       $(this).find('.yx-chart').stop().animate({top: '-96px'});
    });
-   $('.ranking').mousemove(function() {
-       $()
-   })
+
+   $('.hot-set .hot-chart li').mouseenter(function() {
+       $(this).find('.move-hover').stop().animate({height: '150px'});
+   });
+
+    $('.move-hover').hover(function() {
+       $(this).stop().animate({height: '150px'});
+   });
 });
 
 $(document).ready(function () {
-   $('.yx-chart').mouseout(function() {
-       $(this).animate({top: '0'});
+   $('.groom ul li ').mouseleave(function() {
+       $(this).find('.yx-chart').stop().animate({top: '0'});
    });
-   // $('.ranking').hover(function() {
-   //     $(this).find('.imgPack').addClass('hover');
-   // })
+
+   $('.hot-set .hot-chart li').mouseleave(function() {
+       $(this).find('.move-hover').animate({height: '30px'});
+   });
+
+   $('.move-hover').mouseout(function() {
+       $(this).stop().animate({height: '30px'});
+   });
+
+   $('.top-sign li').hover(function() {
+       $(this).children('.imgPack').addClass('hover');
+       $(this).children('.fontPack').addClass('none');
+       $(this).siblings('li').children('.imgPack').removeClass('hover');
+       $(this).siblings('li').children('.fontPack').removeClass('none');
+       $(this).siblings('li').children('.fontPack').removeClass('fontsign');
+       $(this).siblings('li').children('.imgPack').removeClass('imgsign2');
+   });
+
+   $('.yx-newFb li .rj-newFb').hover(function() {
+       $(this).children('.xs').addClass('hover');
+       $(this).children('.lx-rl').addClass('none');
+       $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('hover')
+   });
+
+   $('.hover-').hover(function() {
+       $(this).addClass('hover');
+       $('.hover-').not(this).removeClass('hover')
+   });
+
 });
+
+$(document).ready(function () {
+    $('.yx-newFb li .rj-newFb').hover(function() {
+        $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('come')
+
+    })
+
+});
+
+$(document).ready(function () {
+    $('.yx-newFb li .rj-newFb').hover(function() {
+        $('.yx-newFb li .rj-newFb').not(this).children('.lx-rl').removeClass('none')
+
+    });
+    $('.game-beauty ul li').mouseenter(function() {
+        $(this).find('.tm').stop().animate({opacity: '0'});
+        $('.game-beauty ul li').not(this).find('.tm').stop().animate({opacity: '1'});
+    });
+
+     $('.xz-recall').mouseleave(function() {
+        $(this).find('.game-xz').css('display', 'block');
+        $(this).find('.game-enter').css('display', 'none')
+    });
+
+     $('.hot_article ul li').mouseleave(function() {
+        $(this).find('.name').css('color', '#666')
+    });
+});
+
+$(document).ready(function () {
+    $('.yx-newFb li .rj-newFb').hover(function() {
+        $('.yx-newFb li .rj-newFb').not(this).children('.lx-rl').removeClass('join')
+
+    });
+
+    $('.game-beauty ul li').mouseleave(function() {
+        $(this).find('.tm').animate({opacity: '0'});
+       $('.game-beauty ul li').not(this).find('.tm').animate({opacity: '0'});
+    });
+
+    $('.xz-recall').mouseenter(function() {
+        $(this).find('.game-xz').css('display', 'none');
+        $(this).find('.game-enter').css('display', 'block')
+    });
+
+    $('.hot_article ul li').mouseenter(function() {
+        $(this).find('.name').css('color', '#f60')
+    });
+
+});
+
+
