baf4cef214b51bf1f7fe963396c4bccda826c5ea
yhy <yhy@yhydeMacBook-Pro.local>
2017-06-11 12:35:39 +0800

    优化js网页效果特效

--- static/js/index.js
+++ static/js/index.js
@@ -1,103 +1,108 @@
 /**
  * Created by yhy on 2017/6/5.
  */
+
 $(document).ready(function () {
-   $('.groom ul li').mouseenter(function() {
-       $(this).find('.yx-chart').stop().animate({top: '-96px'});
-   });
-
-   $('.hot-set .hot-chart li').mouseenter(function() {
-       $(this).find('.move-hover').stop().animate({height: '150px'});
-   });
-
-    $('.move-hover').hover(function() {
-       $(this).stop().animate({height: '150px'});
-   });
+    hotCollectionBindEvents(); //热门合集事件绑定
+    recommendBindEvents();//破解游戏推荐
+    latestReleaseBindEvents();//最新发布
+    hotMobilegamesBindEvents();//热门手游事件绑定
+    newhandtravelBindEvents();//最新手游
+    hotArticleBindEvents();//热门文章
+    gamebeautyBindEvents();//游戏美女
+    rankingListBindEvents();//排行榜
 });
 
-$(document).ready(function () {
-   $('.groom ul li ').mouseleave(function() {
-       $(this).find('.yx-chart').stop().animate({top: '0'});
-   });
 
-   $('.hot-set .hot-chart li').mouseleave(function() {
-       $(this).find('.move-hover').animate({height: '30px'});
-   });
-
-   $('.move-hover').mouseout(function() {
-       $(this).stop().animate({height: '30px'});
-   });
-
-   $('.top-sign li').hover(function() {
-       $(this).children('.imgPack').addClass('hover');
-       $(this).children('.fontPack').addClass('none');
-       $(this).siblings('li').children('.imgPack').removeClass('hover');
-       $(this).siblings('li').children('.fontPack').removeClass('none');
-       $(this).siblings('li').children('.fontPack').removeClass('fontsign');
-       $(this).siblings('li').children('.imgPack').removeClass('imgsign2');
-   });
-
-   $('.yx-newFb li .rj-newFb').hover(function() {
-       $(this).children('.xs').addClass('hover');
-       $(this).children('.lx-rl').addClass('none');
-       $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('hover')
-   });
-
-   $('.hover-').hover(function() {
-       $(this).addClass('hover');
-       $('.hover-').not(this).removeClass('hover')
-   });
-
-});
-
-$(document).ready(function () {
-    $('.yx-newFb li .rj-newFb').hover(function() {
-        $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('come')
-
-    })
-
-});
-
-$(document).ready(function () {
-    $('.yx-newFb li .rj-newFb').hover(function() {
-        $('.yx-newFb li .rj-newFb').not(this).children('.lx-rl').removeClass('none')
-
-    });
-    $('.game-beauty ul li').mouseenter(function() {
-        $(this).find('.tm').stop().animate({opacity: '0'});
-        $('.game-beauty ul li').not(this).find('.tm').stop().animate({opacity: '1'});
+function hotCollectionBindEvents(){
+    $('.hot-set .hot-chart .hot-chart-sign').mouseenter(function () {
+        $(this).find('.move-hover').stop().animate({height: '150px'});
     });
 
+    $('.hot-set .hot-chart .hot-chart-sign').mouseleave(function () {
+        $(this).find('.move-hover').animate({height: '30px'});
+    });
+}
+
+function recommendBindEvents(){
+    $('.groom ul li').mouseenter(function() {
+        $(this).find('.yx-chart').stop().animate({top: '-96px'});
+    });
+
+    $('.groom ul li').mouseleave(function() {
+        $(this).find('.yx-chart').stop().animate({top: '0'});
+    });
+}
+
+function latestReleaseBindEvents(){
+     $('.yx-newFb li .rj-newFb').hover(function() {
+         $(this).children('.xs').addClass('hover');
+         $(this).children('.lx-rl').addClass('none');
+         $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('hover');
+         $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('come');
+         $('.yx-newFb li .rj-newFb').not(this).children('.lx-rl').removeClass('join')
+     });
+
+     $('.yx-newFb li .rj-newFb').hover(function() {
+         $(this).children('.xs').addClass('hover');
+         $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('hover');
+         $('.yx-newFb li .rj-newFb').not(this).children('.xs').removeClass('come');
+         $('.yx-newFb li .rj-newFb').not(this).children('.lx-rl').removeClass('none')
+     });
+}
+
+function hotMobilegamesBindEvents(){
      $('.xz-recall').mouseleave(function() {
-        $(this).find('.game-xz').css('display', 'block');
-        $(this).find('.game-enter').css('display', 'none')
-    });
+         $(this).find('.game-xz').css('display', 'block');
+         $(this).find('.game-enter').css('display', 'none')
+     });
 
-     $('.hot_article ul li').mouseleave(function() {
-        $(this).find('.name').css('color', '#666')
-    });
-});
+      $('.xz-recall').mouseenter(function() {
+          $(this).find('.game-xz').css('display', 'none');
+          $(this).find('.game-enter').css('display', 'block')
+      });
+}
 
-$(document).ready(function () {
-    $('.yx-newFb li .rj-newFb').hover(function() {
-        $('.yx-newFb li .rj-newFb').not(this).children('.lx-rl').removeClass('join')
+function newhandtravelBindEvents(){
+      $('.hover-').hover(function() {
+          $(this).addClass('hover');
+          $('.hover-').not(this).removeClass('hover')
+      });
+}
 
-    });
 
-    $('.game-beauty ul li').mouseleave(function() {
-        $(this).find('.tm').animate({opacity: '0'});
-       $('.game-beauty ul li').not(this).find('.tm').animate({opacity: '0'});
-    });
-
-    $('.xz-recall').mouseenter(function() {
-        $(this).find('.game-xz').css('display', 'none');
-        $(this).find('.game-enter').css('display', 'block')
-    });
-
+function hotArticleBindEvents(){
     $('.hot_article ul li').mouseenter(function() {
         $(this).find('.name').css('color', '#f60')
     });
 
-});
+     $('.hot_article ul li').mouseleave(function() {
+         $(this).find('.name').css('color', '#666')
+     });
+}
+
+function gamebeautyBindEvents(){
+     $('.game-beauty ul li').mouseleave(function() {
+         $(this).find('.tm').animate({opacity: '0'});
+         $('.game-beauty ul li').not(this).find('.tm').animate({opacity: '0'});
+     });
+
+      $('.game-beauty ul li').mouseenter(function() {
+          $(this).find('.tm').stop().animate({opacity: '0'});
+          $('.game-beauty ul li').not(this).find('.tm').stop().animate({opacity: '1'});
+      });
+}
+
+function rankingListBindEvents() {
+     $('.top-sign li').hover(function() {
+         $(this).children('.imgPack').addClass('hover');
+         $(this).children('.fontPack').addClass('none');
+         $(this).siblings('li').children('.imgPack').removeClass('hover');
+         $(this).siblings('li').children('.fontPack').removeClass('none');
+         $(this).siblings('li').children('.fontPack').removeClass('fontsign');
+         $(this).siblings('li').children('.imgPack').removeClass('imgsign2');});
+}
+
+
 
 
