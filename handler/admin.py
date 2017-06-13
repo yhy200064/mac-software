7db8ebf9335dfa39d71e4749a2a2d46121e58fe7
yhy <yhy@yhydeMacBook-Pro.local>
2017-06-13 21:07:37 +0800

    新增软件管理

---
+++ handler/admin.py
@@ -0,0 +1,60 @@
+#coding: utf8
+import tornado.web
+import torndb
+from tornado.escape import json_encode
+
+class AdminHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('admin/index.html')
+
+class NewestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('admin/newest.html')
+
+
+class SoftwareHandler(tornado.web.RequestHandler):
+    def post(self):
+        title = self.get_argument('title')
+        type = self.get_argument('type')
+        size = self.get_argument('size')
+        img = self.get_argument('img')
+        edition = self.get_argument('edition')
+        db = torndb.Connection(host = 'localhost', database = 'yhy', user = 'root', password = '88888888')
+        if title == '':
+            self.write({'code': 0})
+            return
+        if type == '':
+            self.write({'code': 0})
+            return
+        if size == '':
+            self.write({'code': 0})
+            return
+        if img == '':
+            self.write({'code': 0})
+            return
+        data = db.insert('insert into software value(%s, %s, %s, %s, %s, %s, %s)', None, title, type, size, img, edition, None)
+        db.close()
+        self.write({'code': 1})
+
+
+class AdminApiNewestHandler(tornado.web.RequestHandler):
+    def get(self):
+        db = torndb.Connection(host='localhost', database='yhy', user='root', password='88888888')
+        softwares = db.query('select * from software order by created_at DESC')
+        result = {}
+        for software in softwares:
+            software['id'] = software.id
+            software['title'] = software.title
+            software['type'] =software.type
+            software['size'] = software.size
+            software['img'] = software.img
+            software['edition'] = software.edition
+            software['created_at'] = software.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        db.close()
+        result['code'] = '0'
+        result['body'] = {'software': softwares}
+        result['message'] = '成功'
+        self.write(json_encode(result))
+
+
+
--- handler/index.py
+++ handler/index.py
@@ -1,10 +1,26 @@
 #coding:utf8
 import tornado.web
+import torndb
+from tornado.escape import json_encode
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
-
-class SoftwareHandler(tornado.web.RequestHandler):
+class ApiNewestHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('我要做软件')
+        db = torndb.Connection(host='localhost', database='yhy', user='root', password='88888888')
+        softwares = db.query('select * from software order by created_at desc limit 8')
+        result = {}
+        for software in softwares:
+            software['id'] = software.id
+            software['title'] = software.title
+            software['type'] = software.type
+            software['size'] = software.size
+            software['img'] = software.img
+            software['edition'] = software.edition
+            software['created_at'] = software.created_at.strftime("%m-%d")
+        db.close()
+        result['code'] = '0'
+        result['body'] = {'software': softwares}
+        result['message'] = '成功'
+        self.write(json_encode(result))
